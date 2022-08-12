cfe53aa4f4469c9571849f12f8c9e5db929a8f06
OfficialPlooob <101042383+OfficialPlooob@users.noreply.github.com>
2022-08-12 16:17:25 -0700

    test

---
+++ COORDS.py
@@ -0,0 +1,6 @@
+# Coords saved in a tuple:
+# from COORDS *
+Zharki = (370,400)
+NorthGeorge = (550,750)
+SouthGeorge = (470,1000)
+Crates = (670,980)
--- main.py
+++ main.py
@@ -2,13 +2,34 @@
 PUBG Map Maker for NA Pro Scrims - 8/12/2022
 """
 import discord
+import asyncio
+import io
+from PIL import Image, ImageFont, ImageDraw
 from keys import token
+from COORDS import *
+
+erangelCopy = Image.open('Erangel.png') # Creating the copy of the map
+title_text = "Test"
+fontsize = 75
+font = ImageFont.truetype("arial.ttf", fontsize)
+image_editable = ImageDraw.Draw(erangelCopy)
+color = (247,191,7)
+image_editable.Imagecolor("#E3FD34")
+image_editable.text((670,980), title_text, fill=255, font=font)
+erangelCopy.save("result.png")
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
+        if message.content.startswith(".map"):
+            with open('result.png', 'rb') as f:
+                picture = discord.File(f)
+                print(picture)
+                await message.channel.send(file=picture)
+
 
 client = MyClient()
 client.run(token)
